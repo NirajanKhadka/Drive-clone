187f6fe55642e48d4ad8009fa8a3bbd7df6ff37c
NirajanKhadka <knirajan1011@gmail.com>
2023-08-11 17:14:34 +0545

    side links completed

--- src/Components/sidenav/Links_Template.jsx
+++ src/Components/sidenav/Links_Template.jsx
@@ -4,12 +4,12 @@ function Links_Template() {
 	return (
 		<ul>
 			{navLinks.sidenavLinks.map(({ title, icon }) => {
-				// ({ title, icon } = e);
-
 				return (
-					<li className="flex gap-2">
-						<i className={`bg-[url('${icon}')] bg-center w-2`}></i>
-						<span>{title}</span>
+					<li className="flex gap-3 justify-start items-center pl-4 text-sm h-8 rounded-2xl hover:bg-slate-200">
+						<i className="w-5 h-5">
+							<img src={icon} alt="" className="w-full h-full object-contain" />
+						</i>
+						<span className="col-start-2 col-end-4">{title}</span>
 					</li>
 				);
 			})}{" "}
